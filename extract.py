213470d395a20e8a25eb6efb6d8e671c8f8bd01b
snarayanank2 <siva@fyle.in>
2021-08-20 22:55:07 +0530

    Fixing how tests are written

---
+++ extract.py
@@ -0,0 +1,22 @@
+from colorama import Fore
+import sys
+import os
+import json
+
+# your import goes here
+import re
+
+
+'''
+    extract_amount
+    input:
+        - dirpath: directory containing receipt and ocr output
+    
+    output:
+        - returns the extracted amount
+
+'''
+def extract_amount(dirpath: str) -> float:    
+    # your logic goes here
+    return 0.0
+
